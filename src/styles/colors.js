a59611c4613d6bc48b65c486b546b8f4a0cf88b8
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-12-30 05:13:30 -0300

    Add colors and global style

---
+++ src/styles/colors.js
@@ -0,0 +1,41 @@
+import { css } from 'styled-components';
+
+export const Colors = {
+  primaryLight: '#4d4d4d',
+  primary: '#1a1a1a',
+  primaryDark: '#000000',
+
+  secondaryLight: '#B8907A',
+  secondary: '#A37258',
+  secondaryDark: '#74513E',
+
+  light: '#EDEDED',
+  lightest: '#FFFFFF',
+
+  dark: '#212121',
+  darkest: '#0D0D0D',
+
+  success: '#27AE60',
+  danger: '#C0392B',
+};
+
+export default css`
+  :root {
+    --primary-light: ${Colors.primaryLight};
+    --primary: ${Colors.primary};
+    --primaryDark: ${Colors.primaryDark};
+
+    --secondaryLight: ${Colors.secondaryLight};
+    --secondary: ${Colors.secondary};
+    --secondaryDark: ${Colors.secondaryDark};
+
+    --light: ${Colors.light};
+    --lightest: ${Colors.lightest};
+
+    --dark: ${Colors.dark};
+    --darkest: ${Colors.darkest};
+
+    --success: ${Colors.success};
+    --danger: ${Colors.danger};
+  }
+`;
---
+++ src/styles/index.js
@@ -0,0 +1,2 @@
+export { default as GlobalStyle } from './global';
+export { Colors } from './colors';

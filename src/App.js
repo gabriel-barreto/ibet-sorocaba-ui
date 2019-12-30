996b88209bac81cc1f3ef18c1b570295d4916b37
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-12-30 05:14:27 -0300

    Integrate with router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react"
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+import { GlobalStyle } from './styles';
+
+import Routes from './routes';
 
 function App() {
-  return <div id="app">App</div>
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          {Routes.map(each => (
+            <Route {...each} />
+          ))}
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;

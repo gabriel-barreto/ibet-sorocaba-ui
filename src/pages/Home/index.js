e20578b5d0ec6b91098758857b007b424c61c932
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-12-31 07:20:46 -0300

    Init home page component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { Layout } from '../../components';
+
 function HomePage() {
-  return <>Home Page</>;
+  return (
+    <Layout>
+      <p>Home Page</p>
+    </Layout>
+  );
 }
 
 export default HomePage;

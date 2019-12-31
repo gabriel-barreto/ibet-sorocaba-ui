4ec9d42472006c2d788dc8354a07be75070e375c
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-12-31 07:18:33 -0300

    Add wrappers to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,29 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { addDecorator, configure } from '@storybook/react';
+import styled from 'styled-components';
+
+import { GlobalStyle } from '../src/styles';
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+`;
+
+const withRouter = storyFn => <BrowserRouter>{storyFn()}</BrowserRouter>;
+
+const withGlobalStyle = storyFn => (
+  <Wrapper>
+    <GlobalStyle />
+    {storyFn()}
+  </Wrapper>
+);
+
+addDecorator(withRouter);
+addDecorator(withGlobalStyle);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);

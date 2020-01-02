16f61f63f42c0dea6bd4c01a171b6eb73ae99799
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-02 01:38:24 -0300

    Add background to navbar stories

--- stories/navbar.stories.js
+++ stories/navbar.stories.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
 
 import Navbar from '../src/components/Navbar';
 
 storiesOf('Navbar', module).add('Default', () => {
-  return <Navbar />;
+  const Background = styled.div`
+    background-color: var(--dark);
+    min-height: 100vh;
+    width: 100%;
+  `;
+
+  return (
+    <Background>
+      <Navbar />
+    </Background>
+  );
 });

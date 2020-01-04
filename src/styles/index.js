998dc8dec414fd1248efb6314c36e73fc29eede4
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-04 00:40:44 -0300

    Export container global style

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,2 +1,3 @@
-export { default as GlobalStyle } from './global';
 export { Colors } from './colors';
+export { default as Container } from './container';
+export { default as GlobalStyle } from './global';

256ffbb506a3585dd2eed5e20f6f38b421c6b37c
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-04 00:37:30 -0300

    Provides featured content to PageTitle component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,23 +5,32 @@ import Navbar from '../Navbar';
 import PageTitle from '../PageTitle';
 import SEO from '../SEO';
 
-function Layout({ children, title }) {
+function Layout({ children, featured, title }) {
   return (
     <>
       <SEO title={title} />
       <header id="app-header">
         <Navbar title={title} />
-        <PageTitle title={title} />
+        <PageTitle title={title} featured={featured} />
       </header>
       <main id="app-main">{children}</main>
     </>
   );
 }
 
-Layout.defaultProps = { title: '' };
+Layout.defaultProps = { title: '', featured: false };
 
 Layout.propTypes = {
   title: PropTypes.string,
+  featured: PropTypes.oneOf([
+    PropTypes.bool,
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      description: PropTypes.arrayOf(PropTypes.string),
+      url: PropTypes.string,
+    }),
+  ]),
   children: PropTypes.node.isRequired,
 };
 

ba6fa385ce27ba865abe95646af172960c101c55
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-04 00:39:41 -0300

    Add IBET Sorocaba page route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-import { Home } from './pages';
+import { IBETSorocaba, Home } from './pages';
 
 export default [
   {
     name: 'home',
     path: '/',
     component: props => <Home {...props} />,
+    exact: true,
+  },
+  {
+    name: 'ibet-sorocaba',
+    path: '/ibet-sorocaba',
+    component: props => <IBETSorocaba {...props} />,
     exact: true,
   },
 ];

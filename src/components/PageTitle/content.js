12e1c1ba0393c3a800efce5d001fbd46193bef81
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-04 00:39:30 -0300

    Set background collection

---
+++ src/components/PageTitle/content.js
@@ -0,0 +1,20 @@
+import homeSm from '../../assets/home-bg-sm.jpg';
+import homeMd from '../../assets/home-bg-md.jpg';
+import homeXl from '../../assets/home-bg-xl.jpg';
+
+import ibetSorocabaSm from '../../assets/ibet-sorocaba-bg-sm.jpg';
+import ibetSorocabaMd from '../../assets/ibet-sorocaba-bg-md.jpg';
+import ibetSorocabaXl from '../../assets/ibet-sorocaba-bg-xl.jpg';
+
+export const bg = {
+  home: {
+    sm: homeSm,
+    md: homeMd,
+    xl: homeXl,
+  },
+  'ibet sorocaba': {
+    sm: ibetSorocabaSm,
+    md: ibetSorocabaMd,
+    xl: ibetSorocabaXl,
+  },
+};

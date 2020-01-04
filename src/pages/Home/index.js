c91dfb216d33c7a4dd20bc6e321189aaf81d0191
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-04 00:33:54 -0300

    Provides static featured content

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 import { Layout } from '../../components';
 
+const featured = {
+  description: [
+    'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos',
+    'Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.',
+  ],
+  label: 'GARANTIR MINHA VAGA',
+  subtitle: 'Lorem Ipsum do Mestrado IBET 2020',
+  title: 'Mestrado IBET 2020',
+  URL: '/',
+};
+
 function HomePage() {
   return (
-    <Layout>
+    <Layout featured={featured}>
       <p>Home Page</p>
     </Layout>
   );

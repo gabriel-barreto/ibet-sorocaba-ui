c1130efbb6b5152c5c063c32966c1e2d1d7a545f
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-04 00:39:13 -0300

    Init IBET Sorocaba page

---
+++ src/pages/IBET-Sorocaba/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Layout } from '../../components';
+
+function IBETSorocabaPage () {
+    return (
+        <Layout title="IBET Sorocaba" featured={false}>
+            IBET Sorocaba Page
+        </Layout>
+    );
+}
+
+export default IBETSorocabaPage;

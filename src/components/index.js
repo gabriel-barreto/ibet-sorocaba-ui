57a2b00e6043ca2a5a708e104769ccf8ebd26640
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-10 05:26:33 -0300

    Export contact section

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Contact } from './Contacts';
 export { default as Layout } from './Layout';
 export { default as ListIntroCards } from './ListIntroCard';
 export { default as Structure } from './Structure';

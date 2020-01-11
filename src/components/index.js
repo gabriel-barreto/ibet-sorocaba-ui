010a9cf6881a31c707aa45f321d9e51f6c8e5a92
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-11 18:00:19 -0300

    Export HTMLContainer component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Footer } from './Footer';
+export { default as HTMLContainer } from './HTMLContainer';
 export { default as Layout } from './Layout';
 export { default as ListIntroCards } from './ListIntroCard';
 export { default as Structure } from './Structure';

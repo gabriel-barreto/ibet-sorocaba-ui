9aa693c23263c00bc58be4a9e1aa3faa2df3af3f
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-12 19:10:21 -0300

    Add title props to routes, add specialization route

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,31 @@
 import React from 'react';
 
-import { IBETBrasil, IBETSorocaba, Home } from './pages';
+import { IBETBrasil, IBETSorocaba, Home, Specialization } from './pages';
 
 export default [
   {
     name: 'home',
+    title: '',
     path: '/',
     component: props => <Home {...props} />,
     exact: true,
   },
   {
+    title: 'IBET Sorocaba',
     name: 'ibet-sorocaba',
     path: '/ibet-sorocaba',
     component: props => <IBETSorocaba {...props} />,
   },
   {
+    title: 'IBET Brasil',
     name: 'ibet-brasil',
     path: '/ibet-brasil',
     component: props => <IBETBrasil {...props} />,
+  },
+  {
+    title: 'Especialização',
+    name: 'especializacao',
+    path: '/especializacao',
+    component: props => <Specialization {...props} />,
   },
 ];

e8bf81ae92875feee6febbd94a32bef3ec07d9e3
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-12 01:11:54 -0300

    Add units section to IBET Brasil page

--- src/pages/IBET-Brasil/index.js
+++ src/pages/IBET-Brasil/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { Layout } from '../../components';
 
 import AboutSection from './About';
+import UnitsSection from './Units';
 
 function IBETBrasilPage() {
   return (
     <Layout title="IBET Brasil">
       <AboutSection />
+      <UnitsSection />
     </Layout>
   );
 }

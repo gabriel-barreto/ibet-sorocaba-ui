c6393b7f9ac2ca2a024b6c16fb19665c04edea90
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-12 19:08:43 -0300

    Export specialization page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './Home';
 export { default as IBETSorocaba } from './IBET-Sorocaba';
 export { default as IBETBrasil } from './IBET-Brasil';
+export { default as Specialization } from './Specialization';

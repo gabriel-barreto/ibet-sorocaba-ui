a646ae25f7ead79f0886f753a13112dcb70829e6
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-11 21:06:34 -0300

    Inject section at page

--- src/pages/IBET-Sorocaba/index.js
+++ src/pages/IBET-Sorocaba/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { Layout } from '../../components';
 
 import AboutSection from './About';
+import CoordinationSection from './Coordination';
 
 function IBETSorocabaPage() {
   return (
     <Layout title="IBET Sorocaba" featured={false}>
       <AboutSection />
+      <CoordinationSection />
     </Layout>
   );
 }

3b34f5967c3a3ab73612f01d23d72b14660c30a8
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-12 19:07:12 -0300

    Export tabs component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as HTMLContainer } from './HTMLContainer';
 export { default as Layout } from './Layout';
 export { default as ListIntroCards } from './ListIntroCard';
 export { default as Structure } from './Structure';
+export { default as Tabs } from './Tabs';

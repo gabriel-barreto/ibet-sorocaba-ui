0eca8ddcdb4a5dcdcc0b766e25edb6c6a7029440
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-12 22:06:47 -0300

    Export input component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as Footer } from './Footer';
 export { default as HTMLContainer } from './HTMLContainer';
+export { default as Input } from './Input';
 export { default as Layout } from './Layout';
 export { default as ListIntroCards } from './ListIntroCard';
 export { default as Structure } from './Structure';

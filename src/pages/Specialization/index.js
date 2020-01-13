08a9667a4b537ee9f8296636afb91822e5790283
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-13 04:20:37 -0300

    Finish specialization page markup

--- src/pages/Specialization/index.js
+++ src/pages/Specialization/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 
 import { Layout, Tabs } from '../../components';
 
+import Form from './Form';
+
 import * as C from './content';
+import * as S from './styled';
 
 function SpecializationPage() {
   return (
     <Layout>
-      <Tabs entries={C.tabs} />
+      <S.SpecializationSectionWrapper>
+        <div>
+          <S.SpecializationTitle>{C.title}</S.SpecializationTitle>
+          <Tabs entries={C.tabs} />
+        </div>
+        <S.SpecializationFormContainer>
+          <S.SpecializationFormTitle>
+            Garanta seu lugar
+          </S.SpecializationFormTitle>
+          <Form onSubmit={() => {}} />
+        </S.SpecializationFormContainer>
+      </S.SpecializationSectionWrapper>
     </Layout>
   );
 }

8dd61e4c6fdaa10cdf8b486bc0bc1b91c03f99a3
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-13 06:01:51 -0300

    Export masters page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Home } from './Home';
 export { default as IBETSorocaba } from './IBET-Sorocaba';
 export { default as IBETBrasil } from './IBET-Brasil';
+export { default as Masters } from './Masters';
 export { default as Specialization } from './Specialization';

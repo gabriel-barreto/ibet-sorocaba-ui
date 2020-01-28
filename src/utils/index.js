fb568f28260bc9ebf462a547191a1ea2e9ff6dd5
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-28 04:28:56 -0300

    Export Object utils methods

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,4 @@ export { default as HTTP } from './http.utils';
 export { default as Icons } from './icons.utils';
 export { default as Media } from './media.utils';
 export { default as String } from './string.utils';
+export { default as $Object } from './object.utils';

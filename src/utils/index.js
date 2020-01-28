6182c3bae62b3494fa791f4f9dd29efe9ebda918
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-28 00:30:54 -0300

    Export icons and http utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,4 @@
+export { default as HTTP } from './http.utils';
+export { default as Icons } from './icons.utils';
 export { default as Media } from './media.utils';
 export { default as String } from './string.utils';

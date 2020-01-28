8567f64362a829b5ea6b8f33eece2d9a75790c00
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-28 04:53:21 -0300

    Export spinner

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ export { default as HTMLContainer } from './HTMLContainer';
 export { default as Input } from './Input';
 export { default as Layout } from './Layout';
 export { default as ListIntroCards } from './ListIntroCard';
+export { default as Spinner } from './Spinner';
 export { default as Structure } from './Structure';
 export { default as Tabs } from './Tabs';

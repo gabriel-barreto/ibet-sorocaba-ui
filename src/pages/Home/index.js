370de80b18f40604aa62f31f1e0cb075ebe3c380
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-01-29 02:04:15 -0300

    Use error prop from hook to redirect use to error page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
 
 import { usePagesContent } from '../../hooks';
-
-import { $Object } from '../../utils';
 
 import { Layout, ListIntroCards, Spinner, Structure } from '../../components';
 
@@ -13,13 +10,13 @@ import WelcomeSection from './Welcome';
 import * as S from './styled';
 
 function HomePage() {
-  const [state, loading] = usePagesContent('home');
+  const [state, loading, error] = usePagesContent('home');
 
   return (
     <Layout
       title="Home"
       featured={state.featured || {}}
-      redirectCondition={$Object.isEmpty(state)}
+      redirectCondition={error}
     >
       <S.SpinnerWrapper visible={loading}>
         <Spinner />

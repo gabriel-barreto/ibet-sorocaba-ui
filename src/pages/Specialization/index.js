ccc595c234c41b588d7859f6b8901483c6d81894
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-02-05 23:29:18 -0300

    Integrate with API using hook

--- src/pages/Specialization/index.js
+++ src/pages/Specialization/index.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Layout, Tabs } from '../../components';
+import { usePagesContent } from '../../hooks';
 
+import {
+  FullScreenSpinner,
+  HTMLContainer,
+  Layout,
+  Tabs,
+} from '../../components';
+
+import Coordination from './Coordination';
 import Form from './Form';
+import Themes from './Theme';
 
-import * as C from './content';
 import * as S from './styled';
 
 function SpecializationPage() {
+  const [state, loading, error] = usePagesContent('specialization');
+  const [localState, setLocalState] = useState({ tabs: [] });
+
+  useEffect(() => {
+    if (state.tabs && state.tabs.length > 0) {
+      const tabs = state.tabs.map(each => ({
+        ...each,
+        component: () => <HTMLContainer content={each.content} />,
+      }));
+      tabs.push({
+        label: 'Coordenação',
+        key: 'coordination',
+        component: () => <Coordination entries={state.coordination} />,
+      });
+
+      setLocalState(prev => ({ ...prev, tabs }));
+    }
+  }, [state.tabs]);
+
   return (
-    <Layout title="Especialização em Direito Tributário do Brasil">
+    <Layout
+      title="Especialização em Direito Tributário do Brasil"
+      redirectCondition={error}
+    >
+      <FullScreenSpinner visible={loading} />
       <S.SpecializationSectionWrapper>
         <div>
-          <S.SpecializationTitle>{C.title}</S.SpecializationTitle>
-          <Tabs entries={C.tabs} />
+          <S.SpecializationTitle>{state.title || ''}</S.SpecializationTitle>
+          <Tabs entries={localState.tabs || []} />
+          <S.SpecializationSubtitle>Temário</S.SpecializationSubtitle>
+          <Themes entries={state.themes || []} />
         </div>
         <S.SpecializationFormContainer>
           <S.SpecializationFormTitle>

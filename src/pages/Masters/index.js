69f0f85c7ad84a5d949d46234bfd33ca30660e32
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-02-05 23:38:45 -0300

    Integrate with API using hook, remove static content

--- src/pages/Masters/index.js
+++ src/pages/Masters/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
-import { HTMLContainer, Layout } from '../../components';
+import { usePagesContent } from '../../hooks';
 
-import * as C from './content';
+import { FullScreenSpinner, HTMLContainer, Layout } from '../../components';
+
 import * as S from './styled';
 
 function MastersPage() {
+  const [state, loading, error] = usePagesContent('masters');
   return (
-    <Layout title="Mestrado">
+    <Layout title="Mestrado" redirectCondition={error}>
+      <FullScreenSpinner visible={loading} />
       <S.MastersSection>
-        <S.MastersSectionTitle>{C.title}</S.MastersSectionTitle>
-        <HTMLContainer content={C.content} />
-        <S.MastersButton href={C.url} target="blank" rel="noreferrer noopener">
+        <S.MastersSectionTitle>{state.title}</S.MastersSectionTitle>
+        <HTMLContainer content={state.content} />
+        <S.MastersButton
+          href={state.url}
+          target="blank"
+          rel="noreferrer noopener"
+        >
           Ver mais informações
         </S.MastersButton>
       </S.MastersSection>

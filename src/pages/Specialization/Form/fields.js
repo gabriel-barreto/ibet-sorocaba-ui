bc13b5972dbe928acc5cd009cd7f34d16563582a
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-02-10 01:52:46 -0300

    Fix phone mask function

--- src/pages/Specialization/Form/fields.js
+++ src/pages/Specialization/Form/fields.js
@@ -21,9 +21,9 @@ export default [
     mask(value) {
       return value
         .replace(/\D/g, '')
-        .replace(/(\d{11})\d/, '')
-        .replace(/(\d{2})\d/, '($1) $2')
-        .replace(/(\d{4,5})\d/, '$1-$2');
+        .replace(/(\d{11})(\d)/, '$1')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4,5})(\d)/, '$1-$2');
     },
     required: true,
   },

6d65d3356cb9ead21067b4df34c48208a9d27526
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-02-10 01:52:20 -0300

    Add className prop to can enhance component style

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,7 +3,16 @@ import PropTypes from 'prop-types';
 
 import * as S from './styled';
 
-function Input({ helper, label, mask, name, onInput, type, ...props }) {
+function Input({
+  className,
+  helper,
+  label,
+  mask,
+  name,
+  onInput,
+  type,
+  ...props
+}) {
   const [value, setValue] = useState('');
 
   function onInputHandler({ target }) {
@@ -18,7 +27,7 @@ function Input({ helper, label, mask, name, onInput, type, ...props }) {
   }
 
   return (
-    <S.InputGroup>
+    <S.InputGroup className={className}>
       {label ? <S.InputLabel htmlFor={name}>{label}:</S.InputLabel> : null}
       {helper ? <S.InputHelper>{helper}</S.InputHelper> : null}
       <S.InputField
@@ -32,8 +41,15 @@ function Input({ helper, label, mask, name, onInput, type, ...props }) {
   );
 }
 
-Input.defaultProps = { helper: '', label: '', mask: false, type: 'text' };
+Input.defaultProps = {
+  className: '',
+  helper: '',
+  label: '',
+  mask: false,
+  type: 'text',
+};
 Input.propTypes = {
+  className: PropTypes.string,
   helper: PropTypes.string,
   label: PropTypes.string,
   mask: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),

8a68183c8442102485b55c2f440b5f11ae0ccc0a
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-02-10 00:04:36 -0300

    Export mailer service

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,3 @@
 export { default as contactInfo } from './contactInfos.service';
 export { default as PagesContent } from './pagesContent.service';
+export { default as Mailer } from './mailer.service';

709961f1312ddefa377a416881b162fd515af3ff
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-02-10 01:54:14 -0300

    Fix input events, work with loading, add disabled condition to button

--- src/pages/Specialization/Form/index.js
+++ src/pages/Specialization/Form/index.js
@@ -1,42 +1,53 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Input } from '../../../components';
+import { $Operators } from '../../../utils';
+
+import { FullScreenSpinner, Input } from '../../../components';
 
 import * as S from './styled';
 
 import fields from './fields';
 
-function SpecializationForm({ onSubmit }) {
+function SpecializationForm({ loading, onSubmit }) {
   const [state, setState] = useState({});
 
   function onInputHandler(key, value) {
-    const actual = state;
-
-    fields[key] = value;
-    return setState(prev => ({ ...prev, ...actual }));
+    return setState(prev => ({
+      ...prev,
+      ...Object.fromEntries([[key, value]]),
+    }));
   }
 
   function onSubmitHandler(e) {
     e.preventDefault();
-    return onSubmit(state);
+
+    const debouncedFn = $Operators.debounce(500, onSubmit);
+    return debouncedFn(state);
   }
 
   return (
     <S.FormWrapper method="POST" onSubmit={onSubmitHandler}>
-      {fields.map(each => (
-        <Input
-          {...each}
-          key={each.name}
-          onInput={value => onInputHandler(each.name, value)}
-        />
-      ))}
-      <S.FormSubmitButton type="Submit">Garantir meu lugar</S.FormSubmitButton>
+      <FullScreenSpinner visible={loading} />
+      {!loading
+        ? fields.map(each => (
+            <S.FormInput
+              {...each}
+              key={each.name}
+              onInput={value => onInputHandler(each.name, value)}
+            />
+          ))
+        : null}
+      <S.FormSubmitButton type="Submit" disabled={loading}>
+        Garantir meu lugar
+      </S.FormSubmitButton>
     </S.FormWrapper>
   );
 }
 
+SpecializationForm.defaultProps = { loading: false };
 SpecializationForm.propTypes = {
+  loading: PropTypes.bool,
   onSubmit: PropTypes.func.isRequired,
 };
 

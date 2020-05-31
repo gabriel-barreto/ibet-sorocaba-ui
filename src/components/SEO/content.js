343a3dc0eb93ed12960fe4a120f8f0d6bc71154f
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-31 00:18:54 -0300

    refactor(SEO): remove unused values

--- src/components/SEO/content.js
+++ src/components/SEO/content.js
@@ -1,7 +1,7 @@
 export default {
   author: '@entrecliques',
-  description: 'Trabalhando para desenvolver o campo do conhecimento jurídico no Brasil',
+  description:
+    'Trabalhando para desenvolver o campo do conhecimento jurídico no Brasil',
   lang: 'pt-br',
   name: 'IBET Sorocaba',
-  meta: [],
 };
--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -4,46 +4,12 @@ import PropTypes from 'prop-types';
 
 import defaultProps from './content';
 
-function SEO({ author, description, lang, meta, name, title }) {
+function SEO({ author, description, lang, name, title }) {
   return (
     <Helmet
       htmlAttributes={{ lang }}
       title={title}
       titleTemplate={title ? `%s | ${name}` : name}
-      meta={[
-        {
-          name: 'description',
-          content: description,
-        },
-        {
-          property: 'og:title',
-          content: title,
-        },
-        {
-          property: 'og:description',
-          content: description,
-        },
-        {
-          property: 'og:type',
-          content: 'website',
-        },
-        {
-          name: 'twitter:card',
-          content: 'summary',
-        },
-        {
-          name: 'twitter:creator',
-          content: author,
-        },
-        {
-          name: 'twitter:title',
-          content: title,
-        },
-        {
-          name: 'twitter:description',
-          content: description,
-        },
-      ].concat(meta)}
     />
   );
 }
